14cc9925bd40277c297d40e4f0614ae68d5ad53e
Jarvis Niu <jarvisniu@gmail.com>
2016-10-27 02:35:36 +0800

    add widget `breadcrumb`

---
+++ vue/breadcrumb.vue
@@ -0,0 +1,30 @@
+<style>
+  @import "vue/breadcrumb.css";
+</style>
+
+<template>
+    <div class="breadcrumb">
+        <div class="breadcrumb-container">
+            <icon-label v-for="item in items" :model="item"></icon-label>
+        </div>
+    </div>
+</template>
+
+<script>
+
+  export default {
+    data: function() {
+      return {
+        items: [
+//        {
+//          icon: 'file.png',
+//          label: "three.js"
+//        }
+        ]
+      }
+    },
+    events: {
+    }
+  }
+
+</script>
